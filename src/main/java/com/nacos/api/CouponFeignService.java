66ce5987275e308c8dbc8f3f9b7f03279c580fc8
zyb <zhaoyunboai@163.com>
2022-10-15 09:34:11 +0800

    github test

--- src/main/java/com/nacos/api/CouponFeignService.java
+++ src/main/java/com/nacos/api/CouponFeignService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient("nacos-provider-demo") // mall-coupon 为调用远程服务的服务名，并且是在nacos的注册列表中可以找到的
 public interface CouponFeignService {
 
+
     @GetMapping("/payment/Hello")  // 远程接口的完整 url 路径
     String SayHelloWord(@RequestParam("key")String key);
 }
